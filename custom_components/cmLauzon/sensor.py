6921a1308ab98ef244a06d70e1a93fdd67fccef8
gecko2308 <60891050+gecko2308@users.noreply.github.com>
2025-05-27 15:03:27 -0400

    Update sensor.py

--- custom_components/cmLauzon/sensor.py
+++ custom_components/cmLauzon/sensor.py
@@ -2,6 +2,7 @@ from homeassistant.helpers.entity import Entity
 from .const import DOMAIN, CONF_USERNAME, CONF_PASSWORD
 from .cmlauzon_api import CMLauzonClient
 import logging
+import aiohttp
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -9,12 +10,15 @@ async def async_setup_entry(hass, entry, async_add_entities):
     username = entry.data[CONF_USERNAME]
     password = entry.data[CONF_PASSWORD]
 
-    client = CMLauzonClient(username, password)
-    if not client.authenticate():
+    session = aiohttp.ClientSession()
+    client = CMLauzonClient(username, password, session)
+
+    if not await client.authenticate():
         _LOGGER.error("Failed to authenticate with CM Lauzon")
+        await session.close()
         return
 
-    data = client.get_availabilities()
+    data = await client.get_availabilities()
 
     sensors = []
     for rdv in data.get("Availabilities", []):
