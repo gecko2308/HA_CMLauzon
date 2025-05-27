ab57845597031d8035e613b0e9d2aa4252aab792
gecko2308 <60891050+gecko2308@users.noreply.github.com>
2025-05-27 14:49:37 -0400

    Update __init__.py

---
+++ custom_components/cmLauzon/__init__.py
@@ -0,0 +1,19 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the CM Lauzon integration from configuration.yaml (optional)."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up CM Lauzon from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
