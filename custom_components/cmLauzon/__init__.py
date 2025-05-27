b215e72e4417352571365aaf95377e2fa3c31a95
gecko2308 <60891050+gecko2308@users.noreply.github.com>
2025-05-27 15:05:54 -0400

    Update __init__.py

--- custom_components/cmLauzon/__init__.py
+++ custom_components/cmLauzon/__init__.py
@@ -1,7 +1,13 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.const import Platform
+
+import aiohttp
+
 from .const import DOMAIN
+
+PLATFORMS: list[Platform] = ["sensor"]
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the CM Lauzon integration from configuration.yaml (optional)."""
@@ -9,11 +15,24 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up CM Lauzon from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    session = aiohttp.ClientSession()
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {
+        "session": session
+    }
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    """Unload a config entry and close session."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+    session = hass.data[DOMAIN][entry.entry_id]["session"]
+    await session.close()
+
+    hass.data[DOMAIN].pop(entry.entry_id)
+    if not hass.data[DOMAIN]:
+        hass.data.pop(DOMAIN)
+
+    return unload_ok
