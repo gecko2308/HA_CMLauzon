fe95935b112894cf98a9133abe4de4d87eb7f8c2
gecko2308 <60891050+gecko2308@users.noreply.github.com>
2025-05-27 15:02:45 -0400

    Update cmlauzon_api.py

--- custom_components/cmLauzon/cmlauzon_api.py
+++ custom_components/cmLauzon/cmlauzon_api.py
@@ -1,16 +1,17 @@
-import requests
+import aiohttp
 import logging
 
 _LOGGER = logging.getLogger(__name__)
 
 class CMLauzonClient:
-    def __init__(self, username, password):
-        self.session = requests.Session()
+    def __init__(self, username, password, session: aiohttp.ClientSession):
+        self.session = session
         self.username = username
         self.password = password
         self.base_url = "https://cmlevislauzon.portail.medfarsolutions.com"
+        self.cookies = None
 
-    def authenticate(self):
+    async def authenticate(self) -> bool:
         _LOGGER.debug("Authenticating with CM Lauzon as %s", self.username)
         url = f"{self.base_url}/auth/credentials"
         payload = {
@@ -20,25 +21,24 @@ class CMLauzonClient:
             "UserName": self.username,
             "Password": self.password
         }
-        response = self.session.post(url, data=payload)
-        response.raise_for_status()
-        if "mp-auth" in self.session.cookies:
-            _LOGGER.debug("Authentication successful")
-        else:
-            _LOGGER.error("Authentication failed")
-        return "mp-auth" in self.session.cookies
 
-    def get_availabilities(self, emergency=False):
+        async with self.session.post(url, data=payload) as response:
+            if response.status != 200:
+                _LOGGER.error("Authentication failed, status code %s", response.status)
+                return False
+
+            self.cookies = response.cookies
+            if "mp-auth" in self.cookies:
+                _LOGGER.debug("Authentication successful")
+                return True
+            else:
+                _LOGGER.warning("No mp-auth cookie received")
+                return False
+
+    async def get_availabilities(self, emergency=False):
         _LOGGER.debug("Fetching appointments (emergency=%s)", emergency)
+        url = f"{self.base_url}/fr/p/0/availability/search"
         params = {
             "appointmentTypeId": "3b3a40d4-ba18-5640-a957-87eba806f607" if emergency else "7be85490-a07c-5374-9b67-44a9f3cbf298",
             "professionalId": "ac1e4218-72bb-594f-a031-e4fd742ce65a",
-            "clinicSiteId": "95a922d2-a721-50fe-9c50-ef48705f7374",
-            "isEmergency": str(emergency).lower(),
-            "startDate": "2025-05-27"
-        }
-        response = self.session.get(f"{self.base_url}/fr/p/0/availability/search", params=params)
-        response.raise_for_status()
-        data = response.json()
-        _LOGGER.debug("Received %d results", data.get("Count", 0))
-        return data
+            "clinicSiteId": "95a
